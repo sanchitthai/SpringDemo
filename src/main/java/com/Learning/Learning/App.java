9a449a7d0d37e41c169426be02c9d2e07b48c0e7
Tarang Khandelwal <tarangkhandelwal@gmail.com>
2019-02-16 15:53:13 +0530

    initial commit

---
+++ src/main/java/com/Learning/Learning/App.java
@@ -0,0 +1,13 @@
+package com.Learning.Learning;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
